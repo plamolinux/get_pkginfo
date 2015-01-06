068b57f905e48a9aaf0e87b4eaddc25ad7530745
KATOH Yasufumi <karma@jazz.email.ne.jp>
2015-01-07 01:31:12 +0900

    Allow to process various types of packages
    * allow to process the package whose name has been changed
    * allow not to process the package that should not been updated

--- get_pkginfo.py
+++ get_pkginfo.py
@@ -1,4 +1,5 @@
 #! /usr/bin/python2
+# -*- coding: euc-jp -*-
 
 import sys, os, subprocess, pickle, argparse
 from urllib2 import urlopen, URLError, HTTPError
@@ -38,6 +39,15 @@ def get_ftp_pkgs(arch):
     newpkgs = pickle.load(response)
     return(newpkgs)
 
+'''
+# for local debugging
+def get_ftp_pkgs2(arch):
+    file = "allpkgs_" + arch + ".pickle"
+    with open(file, 'rb') as f:
+        newpkgs = pickle.load(f)
+    return(newpkgs)
+'''
+
 def download_pkg(url):
     try:
         f = urlopen(url)
@@ -56,24 +66,78 @@ def download_pkg(url):
 def get_args():
     parser = argparse.ArgumentParser(description='Plamo Linux update packages check and download')
     parser.add_argument('-d','--download', action='store_true', help='download package(s)')
+    parser.add_argument('-b','--blocklist',action='store_true', help='ignore block list')
     args = parser.parse_args()
     return args
+
+def check_replaces(orig_list, replaces):
+    for ck in replaces.keys():
+        # print(ck, replaces[ck][0])
+        if orig_list.has_key(ck) == True :
+            (ver, arch, build) = orig_list[ck]
+            if replaces[ck][0] >= ver:
+                del(orig_list[ck])
+                for rep in replaces[ck][1]:
+                    orig_list[rep] = (ver, arch, build)
+
+    return(orig_list)
+
+def rev_replaces(replaces):
+    rev_list = {}
+    for i in replaces.keys():
+        new_pkgs = replaces[i][1]
+        for j in new_pkgs:
+            rev_list[j] = i
+
+    # print(rev_list)
+    return(rev_list)
         
 def main():
+    ''' 
+    my_arch: この環境の arch 名(x86/x86_64)
+    local_pkgs: この環境にインストール済みパッケージのリスト
+    ftp_pkgs: FTPサーバ上にあるパッケージのリスト
+    '''
     my_arch = get_arch()
     local_pkgs = get_localpkgs()
     ftp_pkgs = get_ftp_pkgs(my_arch)
 
     param = get_args()
 
+    '''
+    -b オプションを指定しなければ，ブロックリストに指定したパッケージ
+    (ftp_pkgs['__blockpkgs'])は表示しない(= local_pkgs リストから除く)
+    '''
+    if param.blocklist == False:
+        blocklist = ftp_pkgs['__blockpkgs']
+        for bp in blocklist:
+            # print(bp)
+            del(local_pkgs[bp])
+
+    '''
+    改名，分割，集約したパッケージを追跡する処理．ftp_pkgs['__replaces'] には，該当するパッケージが
+    replace_list['old_name'] = (version, (new_name1, new_name2,,,)) という形で入っており，
+    check_replaces() で，local_pkgs['old_name'] を local_pkgs['new_name1'] = (ver, arch, build)
+    の形に組み直し，ftp_pkgs['new_name1'] と比較して更新対象にする．
+    その際，local_pkgs の old_name は失なわれるので，表示用に rev_list として 'new_name1' -> 'old_name',
+    'new_name2' -> 'old_name' のようなデータを記録しておく．
+    '''
+    replaces = ftp_pkgs['__replaces']
+    rev_list = rev_replaces(replaces)
+    check_pkgs = check_replaces(local_pkgs, replaces)
+    
     for i in local_pkgs.keys():
         try:
             (ver, p_arch, build, path) = ftp_pkgs[i]
             chk = (ver, p_arch, build)
             if local_pkgs[i] != chk:
                 (local_ver, local_arch, local_build) = local_pkgs[i]
-                print("local package:{}-{}-{}-{}".format(i, local_ver, local_arch, local_build))
-                print("new package:{}-{}-{}-{}, path:{}".format(i, ver, p_arch, build, path))
+                if rev_list.has_key(i) == True :
+                    pkgname = rev_list[i]
+                else:
+                    pkgname = i
+                print("local package:{}-{}-{}-{}".format(pkgname, local_ver, local_arch, local_build))
+                print("new package:{}-{}-{}-{}".format(i, ver, p_arch, build))
                 get_path = path.replace("/home/ftp/pub", "")
                 url2 = "ftp://plamo.linet.gr.jp/pub/" + get_path + "/" + i + "-" + ver + "-" + p_arch + "-" + build + ".txz"
                 print(url2)
@@ -81,8 +145,9 @@ def main():
                     download_pkg(url2)
                 print("")
         except KeyError:
-            print("package: {} doesn't exit in FTP tree.".format(i))
-            print("")
+            sys.stderr.write("package: {} doesn't exit in FTP tree.\n".format(i))
+            sys.stderr.write("\n")
 
 if __name__ == "__main__":
     main()
+
