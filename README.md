74fd16d6b9ff80f8d982f50b22b73f7b959e44ea
TAMUKI Shoichi <tamuki@linet.gr.jp>
2015-01-28 13:46:30 +0900

    Various fixes 2
    * Fix try-else clause in make_catlist()
    * Simplify around param.localblock and param.blocklist
    * Add try-else clause around sorted(check_pkgs.keys())
    * Make some small revisions
    * Update usege in README.md
    
    Signed-off-by: TAMUKI Shoichi <tamuki@linet.gr.jp>

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
-Script to check and download the updated package 
+Script to check and download the updated package
 
 ## Usage
 
 ```
-usage: get_pkginfo.py [-h] [-d] [-b]
+usage: get_pkginfo.py [-h] [-d] [-b] [-l LOCALBLOCK]
 
 Plamo Linux update packages check and download
 
 optional arguments:
-  -h, --help       show this help message and exit
-  -d, --download   download package(s)
-  -b, --blocklist  ignore block list
+  -h, --help            show this help message and exit
+  -d, --download        download package(s)
+  -b, --blocklist       ignore block list
+  -l LOCALBLOCK, --localblock LOCALBLOCK
+                        set local blocklist filename
 ```
--- get_pkginfo.py
+++ get_pkginfo.py
@@ -10,7 +10,7 @@ FTP_URL = "ftp://ring.yamanashi.ac.jp/pub/linux/Plamo/Plamo-5.x/"
 #FTP_URL = "ftp://ftp.ring.gr.jp/pub/linux/Plamo/Plamo-5.x/"
 
 def get_args():
-    parser = argparse.ArgumentParser(description =
+    parser = argparse.ArgumentParser(description=
             "Plamo Linux update packages check and download")
     parser.add_argument("-d", "--download",
             action="store_true", help="download package(s)")
@@ -43,7 +43,7 @@ def get_localblock(blockfile):
     with open(blockfile, "r") as f:
         lbs = f.readlines()
     for i in lbs:
-        if len(i.strip()) > 0:
+        if len(i.strip()):
             new_list.append(i.strip())
     return new_list
 
@@ -106,11 +106,12 @@ def make_catlist(remote_pkgs):
             except:
                 print("remote key: {} have illegal data: {}"
                         .format(i, remote_pkgs[i]))
-            dt = path.split("/")
-            cat = dt[-2] if dt[-1].find(".txz") > 0 else dt[-1]
-            tmp_list = catlist[cat] if cat in catlist else []
-            tmp_list.append(i)
-            catlist[cat] = tmp_list
+            else:
+                dt = path.split("/")
+                cat = dt[-2] if dt[-1].find(".txz") > 0 else dt[-1]
+                tmp_list = catlist[cat] if cat in catlist else []
+                tmp_list.append(i)
+                catlist[cat] = tmp_list
     return catlist
 
 def get_local_category(local_pkgs):
@@ -152,13 +153,11 @@ def main():
     if param.localblock:
         if os.path.exists(param.localblock):
             new_block = []
-            local_block = get_localblock(param.localblock)
-            orig_block = ftp_pkgs["__blockpkgs"]
-            for i in orig_block:
+            for i in ftp_pkgs["__blockpkgs"]:
                 new_block.append(i)
-            for i in local_block:
+            for i in get_localblock(param.localblock):
                 new_block.append(i)
-            ftp_pkgs["__blockpkgs"] = (new_block)
+            ftp_pkgs["__blockpkgs"] = new_block
         else:
             print("localblock file: {} doesn't exist.  "
                     "Ignore this option.".format(param.localblock))
@@ -167,10 +166,9 @@ def main():
     (ftp_pkgs["__blockpkgs"])は表示しない(= local_pkgs リストから除く)
     """
     if not param.blocklist:
-        blocklist = ftp_pkgs["__blockpkgs"]
-        for bp in blocklist:
-            del(ftp_pkgs[bp])
+        for bp in ftp_pkgs["__blockpkgs"]:
             del(local_pkgs[bp])
+            del(ftp_pkgs[bp])
     """
     改名したパッケージを追跡するための処理．ftp_pkgs["__replaces"] には，
     該当するパッケージが replace_list["old_name"] = "new_name" という形
@@ -186,6 +184,10 @@ def main():
     for i in sorted(check_pkgs.keys()):
         try:
             (ver, p_arch, build, ext, path) = ftp_pkgs[i]
+        except KeyError:
+            sys.stderr.write("package: {} doesn't exist in FTP tree.\n\n"
+                    .format(i))
+        else:
             chk = (ver, p_arch, build)
             if check_pkgs[i] != chk:
                 (local_ver, local_arch, local_build) = check_pkgs[i]
@@ -209,10 +211,6 @@ def main():
                 if param.download:
                     download_pkg(url2)
                 print("")
-        except KeyError:
-            sys.stderr.write("package: {} doesn't exist in FTP tree.\n"
-                    .format(i))
-            sys.stderr.write("\n")
     """
     新しく追加されたパッケージをチェックする．cat_list{} は，FTP サーバ
     上にあるパッケージを，カテゴリをキーにして，そのカテゴリーに属する
