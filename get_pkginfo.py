06a2690cd735bf02b6f596e6ca9ea63e65500292
KATOH Yasufumi <karma@jazz.email.ne.jp>
2026-04-30 21:28:59 +0900

    ローカルにインストールされたパッケージのみを対象にできるように

--- get_pkginfo.py
+++ get_pkginfo.py
@@ -34,7 +34,10 @@ def get_args():
             help="install downloaded package(s) automatically")
     megrp2.add_argument("-i", "--interactive", action="store_true",
             help="install downloaded package(s) interactively")
-    parser.add_argument("-r", "--reverse", action="store_true",
+    megrp3 = parser.add_mutually_exclusive_group()
+    megrp3.add_argument("-k", "--keep", action="store_true",
+            help="check only installed packages' new version")
+    megrp3.add_argument("-r", "--reverse", action="store_true",
             help="find un-selected package(s)")
     parser.add_argument("-t", "--total", action="store_true",
             help="includes contrib directory")
@@ -104,6 +107,7 @@ def get_confs():
                           "manual"         if param.interactive else ""
     confs["REVERSE"]    = True             if param.reverse     else False
     confs["TOTAL"]      = True             if param.total       else False
+    confs["KEEP"]       = True             if param.keep        else False
     """
     各種設定は，
     引数 > ローカル(~/.pkginfo) > システム(/etc/pkginfo.conf)
@@ -380,6 +384,16 @@ def main():
             if ftp_pkgs[i][4].find("contrib/") >= 0:
                 # print("{} {} deleted".format(i, ftp_pkgs[i]))
                 del(ftp_pkgs[i])
+    """
+    -k オプションがあれば、インストール済みのパッケージ(local_pkgs)以外は対象から除く
+    """
+    if confs["KEEP"] == True:
+        for i in list(ftp_pkgs.keys()) :
+            if i in ["__blockpkgs", "__replaces", "__no_install"]:
+                continue
+            if i not in list(local_pkgs.keys()) :
+                del(ftp_pkgs[i])
+        # breakpoint()
 
     """
     改名したパッケージを追跡するための処理．ftp_pkgs["__replaces"] には，
